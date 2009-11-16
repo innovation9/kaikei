619c86efd23ce2fc049220b33d3619c2b7b69467
juanmpp <juanmpp@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-11-16 06:38:39 +0000

    Agregando las plantillas de las pantallas!!

--- kaikei/frmPlantilla.xaml.cs
+++ kaikei/frmPlantilla.xaml.cs
@@ -11,17 +11,26 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ControlsWPF_JMP.Ventanas;
+using System.Windows.Interop;
+
 
 namespace kaikei
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class frmPlantilla : Window
     {
-        public Window1()
+        public frmPlantilla()
         {
             InitializeComponent();
+        }
+
+        private void Plantilla_Loaded(object sender, RoutedEventArgs e)
+        {
+            GlassVentana wndGlass = new GlassVentana(15, 15, 15, 15, new WindowInteropHelper(this).Handle);
+            wndGlass.AplicarGlass();
         }
     }
 }
