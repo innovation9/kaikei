f1678e8b6600c96d6ee6099ecd32a80c5261f3e1
juanmpp <juanmpp@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-11-23 18:07:47 +0000

    Se agrego la pantalla de Login

---
+++ kaikei/wndLogin.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ControlsWPF_JMP.Ventanas;
+
+namespace Kaikei
+{
+	/// <summary>
+	/// Interaction logic for wndLogin.xaml
+	/// </summary>
+	public partial class wndLogin : WindowJMP
+	{
+		public wndLogin()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
