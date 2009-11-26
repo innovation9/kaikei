109959b9038bd44e3dcad7876fd491fe471c121b
rab4585 <rab4585@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-11-26 04:04:11 +0000

    Agregando Clases.

--- kaikei.core/CEmpleado.cs
+++ kaikei.core/CEmpleado.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace kaikei.core
 {
-    public class Connection
+    class CEmpleado
     {
     }
 }
