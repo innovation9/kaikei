8bcb30a484beb755d419fae49ee97719de864eab
juanmpp <juanmpp@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-11-30 04:33:31 +0000

    Se implemento por completo la validacion de la wndLogin

--- kaikei/wndLogin.xaml.cs
+++ kaikei/wndLogin.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using ControlsWPF_JMP.Ventanas;
+using System.Security;
+using kaikei.core;
 
 namespace Kaikei
 {
@@ -28,6 +30,46 @@ namespace Kaikei
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             txtUsuario.Focus();
+        }
+
+        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            //Comprobamos que usuario y pass no se encuentre vacios
+            if (String.IsNullOrEmpty(txtUsuario.Text.Trim()))
+            {
+                MsjError("ERROR! El campo de 'USUARIO' no puede quedar vacio",
+                    "Campo 'USUARIO' se encuentra vacio");
+                txtUsuario.Focus();
+                return;
+            }
+            if (String.IsNullOrEmpty(txtPassword.Password))
+            {
+                MsjError("ERROR! El campo de 'CONTRASEÑA' no puede quedar vacio",
+                    "Campo 'CONTRASEÑA' se encuentra vacio");
+                txtPassword.Focus();
+                return;
+            }
+
+            //Una vez determinamos que no esten vacios los campos pasamos a comprobar si la contraseña
+            //es correcta
+            if ((txtUsuario.Text == "admin") && (txtPassword.Password == "kaikei"))
+            {
+                MessageBox.Show("Usuario valido");
+            }
+            else
+                MsjError("Lo sentimos, pero el usuario o contraseña digitada es incorrecta, por favor vuelva a intentarlo",
+                    "Usuario o Contraseña invalidas");
+
+        }
+
+        private void MsjError(String MsjError, String MsjTitulo)
+        {
+            MessageBox.Show(MsjError, MsjTitulo, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 	}
 }
