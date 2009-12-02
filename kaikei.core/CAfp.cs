05e83f8524952f0b495fb9e463f246f38448d3e4
rab4585 <rab4585@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-12-02 14:30:12 +0000

    agregando algunas clases

---
+++ kaikei.core/CAfp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kaikei.core
+{
+    class CAfp : IConeccion
+    {
+        CConeccion conexion;
+
+        public int Id_AFP { set; private get; }
+        public String Nombre { set; private get; }
+        public Double Tasa { set; private get; }
+
+        public CAfp()
+        {
+        }
+
+        public CAfp(CConeccion c,int id)
+        {
+            this.conexion = c;
+            this.Id_AFP = id;
+            
+        }
+
+        public int sqlInsert()
+        {
+            try
+            {
+                return conexion.sqlQuery("INSERT INTO AFPS (NOMBRE,TASA) VALUES ('{0}',{1})",
+                    this.Nombre, this.Tasa);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int sqlDelete()
+        {
+            try
+            {
+                return conexion.sqlQuery("DELETE FROM AFPS WHERE ID_AFP = '{0}'", this.Id_AFP);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int sqlUpdate()
+        {
+            try
+            {
+                return conexion.sqlQuery("UPDATE AFPS SET NOMBRE = '{0}' , TASA = {1} WHERE ID_AFP = '{2}'",
+                    this.Nombre, this.Tasa, this.Id_AFP);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
