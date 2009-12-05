82857712634f9e5eb29222c72f414f1935dc1f77
juanmpp <juanmpp@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-12-05 23:04:03 +0000

    Tab de inventario agregado

---
+++ trunk/kaikei/ConnectionDS/InventariosDataProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kaikei.ConnectionDS
+{
+	public class InventariosDataProvider
+	{
+		private static InventariosDS invDS;
+		
+		public static InventariosDS GetInventariosDS {
+			get {
+				if (invDS == null) invDS = new InventariosDS();
+				return invDS;
+			}
+			
+		}
+	}
+}
