eb57a0c0a095b97f7dc972930ef4a32ec97af7f3
rab4585 <rab4585@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-12-07 17:29:59 +0000

    Reporte Balance General y otros.

---
+++ kaikei/VistasTabItem/viewBalanceGeneral.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlTypes;
+using Kaikei.EstadosContablesTableAdapters;
+
+namespace Kaikei.VistasTabItem
+{
+    /// <summary>
+    /// Interaction logic for viewBalanceGeneral.xaml
+    /// </summary>
+    public partial class viewBalanceGeneral : UserControl
+    {
+        rBalanceGeneral bg = new rBalanceGeneral();
+        SqlDateTime FechaInicio;
+        SqlDateTime FechaFin;
+        BALANCE_GENERALTableAdapter Datos;
+        
+
+        public viewBalanceGeneral()
+        {
+            InitializeComponent();
+            Datos =new BALANCE_GENERALTableAdapter();
+
+            EstadosContables ec =new EstadosContables();
+
+            //bg.SetDataSource(Datos.
+            this.crvBalanceGeneral.ReportSource = bg;
+        }
+    }
+}
