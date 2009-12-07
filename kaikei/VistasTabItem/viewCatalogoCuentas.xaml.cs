cb74a1d2712036c8d8450f623bf203d50021f206
juanmpp <juanmpp@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-12-07 21:17:50 +0000

    Agregado el Catalogo de Cuentas

---
+++ kaikei/VistasTabItem/viewCatalogoCuentas.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using Kaikei.ContaGeneralDSTableAdapters;
+
+namespace Kaikei
+{
+	/// <summary>
+	/// Interaction logic for viewCatalogoCuentas.xaml
+	/// </summary>
+	public partial class viewCatalogoCuentas : UserControl
+	{
+        private CLASIFICACIONCUENTASTableAdapter ccTA;
+		public viewCatalogoCuentas()
+		{
+			this.InitializeComponent();
+            ccTA = new CLASIFICACIONCUENTASTableAdapter();
+            ContaGeneralDS cgDS = new ContaGeneralDS();
+            ccTA.Fill(cgDS.CLASIFICACIONCUENTAS);
+            dgCCuentas.ItemsSource = cgDS.CLASIFICACIONCUENTAS.DefaultView;
+		}
+	}
+}
