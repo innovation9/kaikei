0220ecf242d4733c0ce82e91e8ec34e2a682133b
rab4585 <rab4585@e5149382-c9b4-11de-b6dc-25c59a28ecba>
2009-12-07 20:14:35 +0000

    agregando ventana fecha

---
+++ kaikei/wndFechas.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace Kaikei
+{
+    /// <summary>
+    /// Interaction logic for wndFechas.xaml
+    /// </summary>
+    public partial class wndFechas : Window
+    {
+        DateTime fInicio;
+        DateTime fFin;
+
+        public wndFechas()
+        {
+            InitializeComponent();
+        }
+
+        public void Obtener(ref DateTime inicio, ref DateTime fin)
+        {
+            inicio = this.fInicio;
+            fin = this.fFin;            
+        }
+
+        private void txtFechaInicio_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            fInicio = (DateTime)txtFechaInicio.SelectedDate;
+        }
+
+        private void txtFechaFin_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            fFin = (DateTime)txtFechaFin.SelectedDate;
+        }
+
+        private void btnGenerar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+       
+    }
+}
